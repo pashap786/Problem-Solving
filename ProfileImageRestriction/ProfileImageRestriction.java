be5016d8fa31b7972f976ab433afe7ecf62ab4da
Rahul Raj <omrahulrajcse@gmail.com>
2017-07-12 11:09:33 +0530

    adding more problem solving code

---
+++ ProfileImageRestriction/ProfileImageRestriction.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+
+        BufferedReader size = new BufferedReader(new InputStreamReader(System.in));
+        Integer length = Integer.parseInt(size.readLine());
+        Integer number = Integer.parseInt(size.readLine());
+
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<number;i++){
+            list.add(size.readLine());
+        }
+      
+
+        for(String str : list){
+            
+            if(Integer.parseInt(str.split(" ")[0])<length || Integer.parseInt(str.split(" ")[1])<length ){
+                System.out.println("UPLOAD ANOTHER");
+            }
+            else if(Integer.parseInt(str.split(" ")[0]) - Integer.parseInt(str.split(" ")[1]) == 0){
+                System.out.println("ACCEPTED");
+            }
+            else if(Integer.parseInt(str.split(" ")[0])>length && Integer.parseInt(str.split(" ")[1])>length){
+                System.out.println("CROP IT");
+            }
+            else if(Integer.parseInt(str.split(" ")[0])>length || Integer.parseInt(str.split(" ")[1])>length){
+                System.out.println("CROP IT");
+            }
+            else{
+                break;
+            }
+        }
+        
+    }
+}
