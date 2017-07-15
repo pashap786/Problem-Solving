7e7775961c99dc68258e3db30743c2387020f826
Rahul Raj <omrahulrajcse@gmail.com>
2017-07-15 13:31:37 -0400

    Code refactored

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,35 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
 
 //Program to find prime numbers till N, where N is an integer received as stdin.
 class PrimeNumbers {
     public static void main(String args[] ) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        findPrime(number);
+        
+    }
+	
+    public static void findPrime(int number){
         Map<Integer,String> map = new LinkedHashMap<Integer,String>();
-        for(int i=2;i<Integer.parseInt(line);i++)
+        for(int i=2;i<number;i++)
         {
             map.put(i,"unmarked");
         }
        
 	   //change status to 'marked' for all prime numbers.	
-	   for(int i=2;i<map.size();i++){
+	for(int i=2;i<map.size();i++){
          
-            for(Map.Entry<Integer,String> entry : map.entrySet()){
+            	for(Map.Entry<Integer,String> entry : map.entrySet()){
              
-                if(entry.getKey()!=i && entry.getValue()!="marked"){
+                	if(entry.getKey()!=i && entry.getValue()!="marked"){
                     
-                    if( (entry.getKey())%i == 0){
-                        map.put(entry.getKey(), "marked");
-                    }
-                }
-               
+                    		if( (entry.getKey())%i == 0){
+                        		map.put(entry.getKey(), "marked");
+                    		}
+                	}
                 
-            }
+            	}
               
-        }
+          }
         
         for(Map.Entry<Integer,String> entry : map.entrySet()){
             if(entry.getValue()!="marked"){
@@ -37,6 +39,6 @@ class PrimeNumbers {
             }
         }
         
-        
     }
+	    
 }
