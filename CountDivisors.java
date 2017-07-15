e0da02e252b636d06518a2f18a6a7a73d940171b
Rahul Raj <omrahulrajcse@gmail.com>
2017-07-15 19:32:36 +0530

    Code formatted.

--- CountDivisors.java
+++ CountDivisors.java
@@ -7,23 +7,26 @@ class CountDivisors {
     public static void main(String args[] ) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-       
-        List<Integer> list = new ArrayList<Integer>();
-        int count = 0;
-        int dividant = Integer.parseInt(line.split(" ")[2]);
-        for(int i=Integer.parseInt(line.split(" ")[0]);i<=Integer.parseInt(line.split(" ")[1]);i++){
-            list.add(i);
-        }
-        
-        for(Integer str : list){
-            if(str%dividant==0){
-                count++;
-            }
-        }
-     
-        System.out.println(count);
+        String line = br.readLine();  
+        System.out.println(calculate(line));
         
         
     }
+    
+    public static int calculate(String input){
+           List<Integer> list = new ArrayList<Integer>();
+           int count = 0;
+           int dividant = Integer.parseInt(input.split(" ")[2]);
+           for(int i=Integer.parseInt(input.split(" ")[0]);i<=Integer.parseInt(input.split(" ")[1]);i++){
+              list.add(i);
+           }
+        
+           for(Integer str : list){
+              if(str%dividant==0){
+                 count++;
+              }
+           }
+           return count;
+    }
+        
 }
