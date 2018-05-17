5937cc9b3e7bebea30d0d03597c2f29271a6f5d5
Rahul Raj <omrahulrajcse@gmail.com>
2018-05-17 22:49:09 +0530

    Update DeepLearning4j.java

--- DeepLearning4j.java
+++ DeepLearning4j.java
@@ -1,19 +1,29 @@
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
-import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.io.ClassPathResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DeepLearning4j {
+
+    private static Logger log = LoggerFactory.getLogger("DeepLearning4j.class");
+
     public static void main(String[] args) throws IOException, InterruptedException {
+
         Schema schema = new Schema.Builder()
                 .addColumnsString("RowNumber")
                 .addColumnInteger("CustomerId")
@@ -29,15 +39,32 @@ public class DeepLearning4j {
         RecordReader reader = new CSVRecordReader(1,',');
         reader.initialize(new FileSplit(new ClassPathResource("Churn_Modelling.csv").getFile()));
         RecordReader transformProcessRecordReader = new TransformProcessRecordReader(reader,transformProcess);
-//        while(transformProcessRecordReader.hasNext()){
-//            System.out.println("args = " + transformProcessRecordReader.next() + "");
-//        }
+        System.out.println("args = " + transformProcessRecordReader.next() + "");
 
-
-        int labelIndex = 13;
-        int numClasses = 2;
+        int labelIndex = 12;
+        int numClasses = 1;
         int batchSize = 100;
-        DataSetIterator iterator = new RecordReaderDataSetIterator(transformProcessRecordReader,labelIndex,numClasses,batchSize);
+
+        DataSetIterator iterator = new RecordReaderDataSetIterator(transformProcessRecordReader,batchSize,labelIndex,numClasses);
+        List<DataSet> dataList = new ArrayList<>();
+
+        while(iterator.hasNext()){
+             dataList.add(((RecordReaderDataSetIterator) iterator).next());
+        }
+
+        DataSet allData = DataSet.merge(dataList);
+        SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.80);
+
+        DataSet trainSet = testAndTrain.getTrain();
+        DataSet testSet = testAndTrain.getTest();
+
+        DataNormalization dataNormalization = new NormalizerStandardize();
+        dataNormalization.fit(trainSet);
+        dataNormalization.transform(trainSet);
+        dataNormalization.transform(testSet);
+
+        log.info("Building Model------------------->>>>>>>>>");
+
 
     }
 }
