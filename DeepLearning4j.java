b81e240fcbedb9b4cae1c2eb19d6ef90fc74b929
Rahul Raj <omrahulrajcse@gmail.com>
2018-05-18 05:32:10 -0400

    Update DeepLearning4j.java

--- DeepLearning4j.java
+++ DeepLearning4j.java
@@ -1,15 +1,28 @@
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.io.ClassPathResource;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,13 +48,14 @@ public class DeepLearning4j {
         TransformProcess transformProcess = new TransformProcess.Builder(schema)
                                                 .removeColumns("RowNumber","Surname","CustomerId")
                                                 .categoricalToInteger("Gender")
-                                                .categoricalToOneHot("Geography").build();
+                                                .categoricalToOneHot("Geography")
+                                                .removeColumns("Geography[France]")
+                                                .build();
         RecordReader reader = new CSVRecordReader(1,',');
         reader.initialize(new FileSplit(new ClassPathResource("Churn_Modelling.csv").getFile()));
         RecordReader transformProcessRecordReader = new TransformProcessRecordReader(reader,transformProcess);
-        System.out.println("args = " + transformProcessRecordReader.next() + "");
 
-        int labelIndex = 12;
+        int labelIndex = 11;
         int numClasses = 1;
         int batchSize = 100;
 
@@ -49,7 +63,7 @@ public class DeepLearning4j {
         List<DataSet> dataList = new ArrayList<>();
 
         while(iterator.hasNext()){
-             dataList.add(((RecordReaderDataSetIterator) iterator).next());
+             dataList.add(iterator.next());
         }
 
         DataSet allData = DataSet.merge(dataList);
@@ -58,13 +72,38 @@ public class DeepLearning4j {
         DataSet trainSet = testAndTrain.getTrain();
         DataSet testSet = testAndTrain.getTest();
 
+        System.out.println("args = [" + trainSet.get(0) + "]");
+
         DataNormalization dataNormalization = new NormalizerStandardize();
         dataNormalization.fit(trainSet);
         dataNormalization.transform(trainSet);
         dataNormalization.transform(testSet);
 
+        //System.out.println("args = [" + trainSet.get(0) + "]");
         log.info("Building Model------------------->>>>>>>>>");
 
+        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
+                                                    .weightInit(WeightInit.UNIFORM)
+                                                    .activation(Activation.RELU)
+                                                    .updater(new Adam())
+                                                    .list()
+                                                    .layer(new DenseLayer.Builder().nIn(11).nOut(6).build())
+                                                    .layer(new DenseLayer.Builder().nIn(6).nOut(6).build())
+                                                    .layer(new OutputLayer.Builder(LossFunctions.LossFunction.XENT)
+                                                                          .nIn(6).nOut(1).activation(Activation.SIGMOID).build())
+                                                    .backprop(true).pretrain(false)
+                                                    .build();
 
+
+        MultiLayerNetwork multiLayerNetwork = new MultiLayerNetwork(configuration);
+        multiLayerNetwork.init();
+        multiLayerNetwork.setListeners(new ScoreIterationListener());
+        for(int i=0;i<100;i++){
+          multiLayerNetwork.fit(trainSet);
+        }
+        Evaluation evaluation = new Evaluation(1);
+        INDArray output = multiLayerNetwork.output(testSet.getFeatureMatrix());
+        evaluation.eval(testSet.getLabels(),output);
+        System.out.println("args = [" + evaluation.stats() + "]");
     }
 }
