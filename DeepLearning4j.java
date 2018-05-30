e4994c4dd66e6cbe8006c0c5193e8707e8ad22ee
Rahul Raj <omrahulrajcse@gmail.com>
2018-05-30 09:33:04 +0530

    Update DeepLearning4j.java

--- DeepLearning4j.java
+++ DeepLearning4j.java
@@ -5,7 +5,7 @@ import org.datavec.api.split.FileSplit;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.datasets.iterator.DataSetIteratorSplitter;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -14,12 +14,7 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.io.ClassPathResource;
 import org.nd4j.linalg.learning.config.Adam;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
@@ -27,16 +22,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class DeepLearning4j {
 
     private static Logger log = LoggerFactory.getLogger("DeepLearning4j.class");
-    
+
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        //Schema Definitions
         Schema schema = new Schema.Builder()
                 .addColumnsString("RowNumber")
                 .addColumnInteger("CustomerId")
@@ -45,67 +39,60 @@ public class DeepLearning4j {
                 .addColumnCategorical("Geography",Arrays.asList("France","Spain","Germany"))
                 .addColumnCategorical("Gender",Arrays.asList("Male","Female"))
                 .addColumnsInteger("Age","Tenure","Balance","NumOfProducts","HasCrCard","IsActiveMember","EstimatedSalary","Exited").build();
+
+        //Schema Transformation
         TransformProcess transformProcess = new TransformProcess.Builder(schema)
-                                                .removeColumns("RowNumber","Surname","CustomerId")
-                                                .categoricalToInteger("Gender")
-                                                .categoricalToOneHot("Geography")
-                                                .removeColumns("Geography[France]")
-                                                .build();
+                .removeColumns("RowNumber","Surname","CustomerId")
+                .categoricalToInteger("Gender")
+                .categoricalToOneHot("Geography")
+                .removeColumns("Geography[France]")
+                .build();
+
+        //CSVReader - Reading from file and applying transformation
         RecordReader reader = new CSVRecordReader(1,',');
         reader.initialize(new FileSplit(new ClassPathResource("Churn_Modelling.csv").getFile()));
         RecordReader transformProcessRecordReader = new TransformProcessRecordReader(reader,transformProcess);
 
-        int labelIndex = 11;
+        int labelIndex = 11;  // consider index 0 to 11  for input
         int numClasses = 1;
         int batchSize = 10;
 
         DataSetIterator iterator = new RecordReaderDataSetIterator(transformProcessRecordReader,batchSize,labelIndex,numClasses);
-        List<DataSet> dataList = new ArrayList<>();
+        DataSetIteratorSplitter splitter = new DataSetIteratorSplitter(iterator,10000,0.8);
 
-        while(iterator.hasNext()){
-             dataList.add(iterator.next());
-        }
-
-        DataSet allData = DataSet.merge(dataList);
-        SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.80);
-
-        DataSet trainSet = testAndTrain.getTrain();
-        DataSet testSet = testAndTrain.getTest();
-
-        System.out.println("args = [" + trainSet.get(0) + "]");
-
-        DataNormalization dataNormalization = new NormalizerStandardize();
-        dataNormalization.fit(trainSet);
-        dataNormalization.transform(trainSet);
-        dataNormalization.transform(testSet);
-
-        //System.out.println("args = [" + trainSet.get(0) + "]");
         log.info("Building Model------------------->>>>>>>>>");
 
         MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
-                                                    .updater(new Adam(0.0001D))
-                                                    .list()
-                                                    .layer(new DenseLayer.Builder().nIn(11).nOut(6).weightInit(WeightInit.UNIFORM).activation(Activation.RELU).dropOut(0.9).build())
-                                                    .layer(new DenseLayer.Builder().nIn(6).nOut(6).weightInit(WeightInit.UNIFORM).activation(Activation.RELU).dropOut(0.9).build())
-                                                    .layer(new OutputLayer.Builder(LossFunctions.LossFunction.XENT)
-                                                                          .nIn(6).nOut(1).weightInit(WeightInit.UNIFORM).activation(Activation.SIGMOID).build())
-                                                    .backprop(true).pretrain(false)
-                                                    .build();
+                .updater(new Adam(0.0001D))
+                .list()
+                .layer(new DenseLayer.Builder().nIn(11).nOut(6).weightInit(WeightInit.UNIFORM).activation(Activation.RELU).dropOut(0.9).build())
+                .layer(new DenseLayer.Builder().nIn(6).nOut(6).weightInit(WeightInit.UNIFORM).activation(Activation.RELU).dropOut(0.9).build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.XENT)
+                        .nIn(6).nOut(1).weightInit(WeightInit.UNIFORM).activation(Activation.SIGMOID).build())
+                .backprop(true).pretrain(false)
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(configuration);
+        model.init();
+        model.setListeners(new ScoreIterationListener(100));
+        model.fit(splitter.getTrainIterator(),10);
 
 
-        MultiLayerNetwork multiLayerNetwork = new MultiLayerNetwork(configuration);
-        multiLayerNetwork.init();
-        multiLayerNetwork.setListeners(new ScoreIterationListener());
-       // DataSetIterator kFoldIterator = new KFoldIterator(trainSet);
-        //multiLayerNetwork.fit(kFoldIterator,100);
-        for(int i=0;i<100;i++){
-            multiLayerNetwork.fit(trainSet);
-        }
-        
+
+/*
         Evaluation evaluation = new Evaluation(1);
-        INDArray output = multiLayerNetwork.output(testSet.getFeatureMatrix());
-        //output = output.cond(new AbsValueGreaterThan(0.50));
+        INDArray output = model.output(splitter.getTestIterator());
+       // output = output.cond(new AbsValueGreaterThan(0.50));
+        DataSetIterator splitIterator = splitter.getTestIterator();
+        List<DataSet> dataset = new ArrayList<>();
+        while(iterator.hasNext()){
+             dataset.add(splitIterator.next());
+        }
+        DataSet testSet = DataSet.merge(dataset);
         evaluation.eval(testSet.getLabels(),output);
-        System.out.println("args = [" + evaluation.stats() + "]");
+        System.out.println("args = " + evaluation.stats() + "");
+*/
+
+
     }
 }
